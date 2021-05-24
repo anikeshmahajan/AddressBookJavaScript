b8c20dbaf026984606f9660cd721a8452e4bb453
Anikesh <anikesh@0725@gmail.com>
2021-05-24 22:33:15 +0530

    Addition : Adding Switch Case UnitChecker

---
+++ UnitCheckUsingSwitch.js
@@ -0,0 +1,14 @@
+function unitCheck(num){
+    switch(num){
+        case 1: console.log("Unit"); break;
+        case 10: console.log("Ten"); break;
+        case 100: console.log("Hundred"); break;
+        case 1000: console.log("Thousand"); break;
+        case 10000: console.log("Ten Thousand"); break;
+        default: console.log("Invalid Number");
+    }
+}
+unitCheck(1);
+unitCheck(100);
+unitCheck(999);
+unitCheck(10000);
