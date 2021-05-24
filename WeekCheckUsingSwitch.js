d3e7f8bc4c30a8bbb8cdb8691a8289b22345387d
Anikesh <anikesh@0725@gmail.com>
2021-05-24 22:32:34 +0530

    Addition : Adding Switch Case WeekChecker

---
+++ WeekCheckUsingSwitch.js
@@ -0,0 +1,15 @@
+function weekCheck(day){
+    switch(day){
+        case 7: console.log("Sunday"); break;
+        case 1: console.log("Monday"); break;
+        case 2: console.log("Tuesday"); break;
+        case 3: console.log("Wednesday"); break;
+        case 4: console.log("Thursday"); break;
+        case 5: console.log("Friday"); break;
+        case 6: console.log("Saturday"); break;
+        default: console.log("No Such Day");
+    }
+}
+weekCheck(4);
+weekCheck(5);
+weekCheck(15);
