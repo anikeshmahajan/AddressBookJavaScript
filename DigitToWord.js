c94d384a259e051ae993a5d96962117ff29d4b1d
Anikesh <anikesh@0725@gmail.com>
2021-05-24 22:28:07 +0530

    Addition : Adding If_ELSE_IF DigitToWord

---
+++ DigitToWord.js
@@ -0,0 +1,50 @@
+function wordCheck(word)
+{
+    if (word == 0) {
+        console.log("The Word Is " + "Zero")
+    } 
+        else if(word ==1)
+        {
+            console.log("The Word Is " + "One")
+        }
+        else if(word ==2)
+        {
+            console.log("The Word Is " + "Two")
+        }
+        else if(word ==3)
+        {
+            console.log("The Word Is " + "Three")
+        }
+        else if(word ==4)
+        {
+            console.log("The Word Is " + "Four")
+        }
+        else if(word ==5)
+        {
+            console.log("The Word Is " + "Five")
+        }
+        else if(word ==6)
+        {
+            console.log("The Word Is " + "Six")
+        }
+        else if(word ==7)
+        {
+            console.log("The Word Is " + "Seven")
+        }
+        else if(word ==8)
+        {
+            console.log("The Word Is " + "Eight")
+        }
+        else if(word ==9)
+        {
+            console.log("The Word Is " + "Nine")
+        }
+    else{
+        console.log("Invalid entry")
+    }
+
+
+}
+wordCheck(1);
+wordCheck(9)
+wordCheck(12)
